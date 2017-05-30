6dfff3c38c21f4216b23b91059ec92aaaaf6203b
adriano <adriano.henriquesds@gmail.com>
2017-05-29 23:06:24 -0300

    Classe inicial de testes, mas sem função ainda

---
+++ src/test/java/com/inovacenter/agendamento/model/EstabelecimentoTest.java
@@ -0,0 +1,15 @@
+package com.inovacenter.agendamento.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstabelecimentoTest {
+	
+	@Test
+	public void contextLoads() {
+	}
+}
